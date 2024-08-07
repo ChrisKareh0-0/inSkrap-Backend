da25db2de6f701dc19cd82c3d8b84f6cc81a2bb4
Chris <chriskareh@proton.me>
2024-08-07 18:02:12 +0300

    production errors fix

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def scrape():
     return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=port, debug=True)
